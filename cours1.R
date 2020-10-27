50a0a89f32a6a7b97e83be26863f309cd23a633d
MonkeyDflo <florent.jakubowki@gmail.com>
2020-10-27 15:58:43 -0700

    Ajout projet et cours 1

---
+++ cours1.R
@@ -0,0 +1,12 @@
+#Model Based Learning
+#Cours 1 
+#Florent Jakubowski
+#20201027
+
+#Exercice 1 ####
+
+#Exercice 2 ####
+#input : (x1, y1), ..., (xn, yn)
+# dnv norm = densité gaussienne multivariée = fk en un point x multivariée
+#d'espérance k et ... k
+#output la classe prédite
